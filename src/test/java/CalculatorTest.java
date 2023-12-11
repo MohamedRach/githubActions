71ca7fef3bb57f5e112f905e8ea1a53d2659cacd
MohamedRach <rachidi.mohamed.sadek@gmail.com>
2023-12-11 13:06:48 +0100

    starter files

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    public void testAddition() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtraction() {
+        assertEquals(2, calculator.subtract(5, 3));
+    }
+
+    @Test
+    public void testMultiplication() {
+        assertEquals(12, calculator.multiply(3, 4));
+    }
+
+    @Test
+    public void testDivision() {
+        assertEquals(2, calculator.divide(6, 3));
+    }
+
+    @Test
+    public void testDivisionByZero() {
+        assertThrows(IllegalArgumentException.class, () -> calculator.divide(5, 0));
+    }
+}
