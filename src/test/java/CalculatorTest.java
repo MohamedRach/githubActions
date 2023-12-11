0336efc3ba60e902ace62cd9b34d6e81e0936864
MohamedRach <rachidi.mohamed.sadek@gmail.com>
2023-12-11 13:29:27 +0100

    solving error

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -22,7 +22,7 @@ public class CalculatorTest {
 
     @Test
     public void testDivision() {
-        assertEquals(3, calculator.divide(6, 3));
+        assertEquals(2, calculator.divide(6, 3));
     }
 
     @Test
